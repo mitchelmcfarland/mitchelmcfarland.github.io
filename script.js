d9e0492443575483c000e3eea94ff59a283eec68
mitchelmcfarland <143099917+mitchelmcfarland@users.noreply.github.com>
2025-07-17 21:49:48 -0400

    testing dark mode toggle

---
+++ script.js
@@ -0,0 +1,15 @@
+if (localStorage.getItem("theme") == "dark") {
+    document.body.classList.add('dark-mode');
+}
+
+
+document.getElementById("theme-toggle").addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+});
+
